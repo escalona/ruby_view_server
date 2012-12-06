63ce057f8a4a51a49f51b558ace0f9a11313d95d
Aaron Escalona <escalona.ap@gmail.com>
2012-12-05 16:53:37 -0800

    Added name and number examples

--- erb_example.rb
+++ erb_example.rb
@@ -5,5 +5,12 @@ template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
 
 pets = ['max', 'ruby', 'spike']
-names = ERB.new "I have three pets named <% pets.each do |pet| %><%= pet.capitalize %>, <% end %>."
-puts names.result(binding)
+pet_names = ERB.new "I have three pets named <% pets.each do |pet| %><%= pet.capitalize %>, <% end %>."
+puts pet_names.result(binding)
+
+numbers = ERB.new "15 + 45 = <%= 15 + 45 %>"
+puts numbers.result(binding)
+
+full_name = "John Doe"
+name = ERB.new "Hi, my name is <%= full_name %>"
+puts name.result(binding)
