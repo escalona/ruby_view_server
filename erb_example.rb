9d2c2e2474e9d0d348dd1d72a2b43a2b3afea76b
Aaron Escalona <escalona.ap@gmail.com>
2012-12-05 16:48:45 -0800

    Added pet names array

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,7 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+pets = ['max', 'ruby', 'spike']
+names = ERB.new "I have three pets named <% pets.each do |pet| %><%= pet.capitalize %>, <% end %>."
+puts names.result(binding)
